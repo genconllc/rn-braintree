e2859458ac2fa47020a243b7a6fcc14576fced6c
Arpan Sharma <arpansharma@zapbuild.com>
2020-10-09 16:26:15 +0530

    Task - working on android set up

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,23 +1,22 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import RnBraintree from 'rn-braintree';
+import { StyleSheet, View, Button } from 'react-native';
+// import RnBraintree from 'rn-braintree';
 
 export default function App() {
 
   async function onButtonPress() {
-    try {
-      const res = await RnBraintree.setupBT("check")
-      console.log(res, "response")
-    } catch (error) {
-      console.log(error, "response")
-    }
+    // try {
+    //   const res = await RnBraintree.setupBT('check')
+    //   console.log(res, 'response')
+    // } catch (error) {
+    //   console.log(error, 'response')
+    // }
   }
 
   return (
     <View style={styles.container}>
       <Button
-        onPress={onButtonPress}
-        title={"SetUp"}
+        onPress={onButtonPress}title={'SetUp'}
       />
     </View>
   );
